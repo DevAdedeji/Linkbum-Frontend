925423ababf51fb340ef1c8db72ebd8d269aa9a9
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-24 01:31:49 +0100

    Done with home page

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="py-4">
+    <div class="flex items-center justify-between w-[90%] mx-auto">
+      <div class="logo">
+        <h1 class="font-black text-3xl">
+          Linkbum<span class="text-green text-5xl">.</span>
+        </h1>
+      </div>
+      <div class="flex items-center gap-5 text-xl">
+        <router-link
+          :to="{ name: 'login' }"
+          class="border border-green rounded-[10px] h-[48px] flex items-center justify-center px-5 hover:bg-green hover:text-[#fff]"
+          >Log in</router-link
+        >
+        <router-link
+          :to="{ name: 'register' }"
+          class="bg-green text-[#fff] rounded-[10px] h-[48px] flex items-center justify-center px-5 hover:border hover:border-green hover:text-[#000] hover:bg-transparent"
+          >Sign Up</router-link
+        >
+      </div>
+    </div>
+  </header>
+</template>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,11 +1,24 @@
 <template>
-    <div class="flex">
-        <div>
-           Home
-        </div>
+  <div class="min-h-screen bg-[#ffff] h-screen">
+    <Header class="pb-40" />
+    <div class="w-full flex flex-col items-center justify-center">
+      <h1
+        class="text-5xl md:text-7xl font-bold text-center text-dark max-w-[700px] mx-auto pb-5"
+      >
+        The Only <span class="text-green">Link</span> You'll Ever Need
+      </h1>
+      <p class="text-xl font-black pb-5">
+        Connect audiences to all of your content with just one link
+      </p>
+      <router-link
+        :to="{ name: 'login' }"
+        class="h-[48px] bg-green rounded-[10px] text-[#ffff] flex items-center justify-center px-5"
+        >Get Started</router-link
+      >
     </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-
+import Header from "../components/Header.vue";
 </script>
