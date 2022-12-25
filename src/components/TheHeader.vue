4d4058e83d955161f43d8668c937f82a7061d36e
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-25 22:03:45 +0100

    Working on the dashboard

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div
+    class="w-[95%] h-[10vh] flex flex-col justify-center mx-auto bg-[#fff] rounded-[20px] px-4"
+  >
+    <header class="flex items-center justify-between">
+      <div class="logo">
+        <h1 class="font-black text-3xl">
+          Linkbum<span class="text-green text-5xl">.</span>
+        </h1>
+      </div>
+    </header>
+  </div>
+</template>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,8 +1,11 @@
 <template>
-  <h1>Dashboard</h1>
+  <div class="bg-grey min-h-screen w-full py-2">
+    <TheHeader />
+  </div>
 </template>
 
 <script lang="ts" setup>
 import { useTitle } from "vue-page-title";
+import TheHeader from "../components/TheHeader.vue";
 const { title } = useTitle("Dashboard");
 </script>
