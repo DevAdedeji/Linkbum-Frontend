0f9e20d1b56d2db5f2e51b5f60049e48d5a250ca
DevAdedeji <adedejitewogbade2@gmail.com>
2022-12-31 12:02:31 +0100

    Working on settings

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -6,7 +6,43 @@
       <div class="py-6">
         <h2 class="text-3xl text-center">My Account</h2>
       </div>
-      <div></div>
+      <div class="Information">
+        <form
+          class="flex flex-col gap-4 bg-[#fff] w-[90%] md:w-[70%] lg:w-[50%] mx-auto rounded-[10px] p-5"
+          @submit.prevent="updateUserInfo"
+        >
+          <div class="flex flex-col gap-2">
+            <p class="text-[12px]">Username</p>
+            <input
+              type="text"
+              v-model="user.username"
+              class="capitalize border-b border-[#e9e9e9] pb-2 outline-none"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <p class="text-[12px]">Email</p>
+            <input
+              type="text"
+              v-model="user.email"
+              class="border-b border-[#e9e9e9] pb-2 outline-none"
+            />
+          </div>
+          <div class="flex flex-col gap-2">
+            <p class="text-[12px]">Bio</p>
+            <textarea
+              id=""
+              cols="10"
+              rows="5"
+              class="outline-none border border-[#e9e9e9] p-2"
+              v-model="user.bio"
+              >{{ user.bio }}</textarea
+            >
+          </div>
+          <button class="rounded-[10px] bg-green text-[#fff] py-3 w-[30%]">
+            Save Details
+          </button>
+        </form>
+      </div>
     </section>
     <!-- <form @submit.prevent="upload" class="pt-4 sm:pt-[10vh]">
       <input type="file" accept="image/*" id="profilePic" />
@@ -16,7 +52,6 @@
 </template>
 
 <script lang="ts" setup>
-import axios from "axios";
 import { ref } from "vue";
 import { useRouter } from "vue-router";
 import TheHeader from "../components/TheHeader.vue";
@@ -34,13 +69,8 @@ const loading = ref<boolean>(true);
 let user = ref({
   username: "",
   profilePic: "",
-  links: [
-    {
-      title: "",
-      link: "",
-      _id: "",
-    },
-  ],
+  email: "",
+  bio: "",
 });
 const getUserData = () => {
   getData(`api/user/me/${id}`)
@@ -63,6 +93,10 @@ const getUserData = () => {
       }
     });
 };
+
+const updateUserInfo = () => {};
+
+getUserData();
 
 const upload = async (e: Event) => {
   // let file = document.getElementById("profilePic")?.files[0];
