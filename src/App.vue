a9d85ed9828479ba9d894a32f1b53853b9dc7132
DevAdedeji <adedejitewogbade2@gmail.com>
2023-02-05 11:48:46 +0100

    Added the function that checks if there is internet connection or not

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,31 @@
 <template>
+  <div v-if="!isOnLine" class="bg-red-500 text-white py-3 text-center">
+    No internet connection
+  </div>
   <router-view></router-view>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onBeforeMount, ref, onBeforeUnmount, watch } from "vue";
+
+const isOnLine = ref<boolean>(true);
+
+onBeforeMount(() => {
+  window.addEventListener("online", () => {
+    isOnLine.value = true;
+  });
+  window.addEventListener("offline", () => {
+    isOnLine.value = false;
+  });
+});
+onBeforeUnmount(() => {
+  window.removeEventListener("online", () => {});
+  window.removeEventListener("offline", () => {});
+});
+
+watch(isOnLine, (newVal) => {
+  isOnLine.value = newVal;
+});
+</script>
 
 <style scoped></style>
