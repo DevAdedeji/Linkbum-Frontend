a91227eb2ba4f984455dd5b2211e278405e71879
DevAdedeji <adedejitewogbade2@gmail.com>
2023-11-26 23:21:43 +0100

    Hope it works

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,10 +9,9 @@ module.exports = {
     extend: {
       colors:{
         green:'#3DA764',
-        grey:'#F2F3F2',
+        grey:'#F9F9F9',
         'grey-2':'#F2F3F2',
-        primary: '#867fff',
-        dark_primary: '#6C63FF',
+        primary: '#6C63FF',
       }
     },
   },
